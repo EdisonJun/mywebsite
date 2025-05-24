54030a2316d976a615612998a8dce58e8bcba408
Ed1s0nZhu <juanyanz@usc.edu>
2025-05-24 16:47:30 -0700

    update server.js_v2

--- server.js
+++ server.js
@@ -6,28 +6,24 @@ const path = require("path");
 const app = express();
 const PORT = 4000;
 
+const strapi_api = "https://wonderful-happiness-d64b2f25c1.strapiapp.com/";
+
 // Token 请勿前端暴露，仅服务端使用
 const Token =
   "daf95a6223078366798bc9346eade59a7a07fc6b86110910597e327bd834ce8025ee5559887d88b4541551ef3b54cbc4ada7cae630e08953a68bd082a938cf7fb649e7457c95a5249eb9051f422f7cb486637b6689f53767eb7f9e0b475deefce2eaf9d4e4742c6252a1153cb86f73146de795261ae963dd19513c8d692c303a";
 
 app.use(cors()); // 允许跨域请求
 
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "../public/uploads"))
-);
+app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
 
 // 获取技能列表
 app.get("/skills", async (req, res) => {
   try {
-    const response = await fetch(
-      "http://localhost:1337/api/skills?populate=image",
-      {
-        headers: {
-          Authorization: `Bearer ${Token}`,
-        },
-      }
-    );
+    const response = await fetch(strapi_api + "/api/skills?populate=image", {
+      headers: {
+        Authorization: `Bearer ${Token}`,
+      },
+    });
 
     if (!response.ok) {
       const err = await response.text();
@@ -45,7 +41,7 @@ app.get("/skills", async (req, res) => {
 // 获取文章列表
 app.get("/articles", async (req, res) => {
   try {
-    const response = await fetch("http://localhost:1337/api/articles", {
+    const response = await fetch(strapi_api + "/api/articles", {
       headers: {
         Authorization: `Bearer ${Token}`,
       },
@@ -68,7 +64,7 @@ app.get("/articles", async (req, res) => {
 app.get("/projects", async (req, res) => {
   try {
     const response = await fetch(
-      "http://localhost:1337/api/projects?sort=publishedAt:desc&populate=*",
+      strapi_api + "/api/projects?sort=publishedAt:desc&populate=*",
       {
         headers: {
           Authorization: `Bearer ${Token}`,
